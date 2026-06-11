cbd7f5923cfb62dd6036e986505f353523e3ccd5
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 01:03:49 +0300

    Update to 26.1 and apply devextreme-examples-template 26.1.3+ changes

--- Vue/src/main.ts
+++ Vue/src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue';
+import config from 'devextreme/core/config';
 import App from './App.vue';
+import { licenseKey } from './devextreme-license';
+
+config({ licenseKey });
 
 createApp(App).mount('#app');
